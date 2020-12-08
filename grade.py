db25a4a2f4527692b8fed866164291a74a86d4f0
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2020-12-08 12:19:31 +0700

    Implement function print_grade_to_file in grade.py. Print result in grade-file.txt

--- grade.py
+++ grade.py
@@ -82,6 +82,30 @@ def print_grade_to_file(dict_grade, dict_activity):
     print('----Fungsi "print_grade_to_file" dijalankan----')
     #jawaban anda di bawah ini
 
+    file_name = input("Masukkan nama file: ")
+
+    file_object = open(file_name, "a+")
+    for key, value in dict_grade.items():
+        # if file exist, clear all data
+        file_object.truncate(0)
+
+        # print id into grade-file.txt
+        file_object.write(f"ID\t\t: {key}\n")
+
+        # print activity title into grade-file.txt
+        file_object.write(f"Title\t: {dict_activity[key]['Title']}\n")
+
+        # print nilai format
+        file_object.write("Nilai semua mahasiswa:\n")
+        file_object.write("NIM\t\t|Nilai\t\t\n")
+        file_object.write("-"*20 + "\n")
+
+        # loop grade nilai
+        for nim, nilai in dict_grade[key].items():
+            # print nim and nilai into grade-file.txt
+            file_object.write(f"{nim}\t\t|{nilai}\t\t\n")
+    file_object.close()
+
     
 
 if __name__ == "__main__":
@@ -89,40 +113,67 @@ if __name__ == "__main__":
     NIM_LOGIN = ''
     ADMIN_LOGIN = False
 
+    # key pada dict_mhs['data'] adalah NIM
+    dict_mhs = {
+        "field": [
+            ("Nama", "^([a-zA-Z] ([ '-]| ')?[a-zA-Z] ){1,4}$"),
+            ("Email", "^([a-z0-9] [._]?[a-z0-9] ) [@]\w [.]\w{2,3}"),
+            ("Password", "^[a-zA-Z0-9]{8,12}$"),
+        ],
+        "data": {
+            "113": {"Nama": "Dummy", "Email": "dummy@telU.com", "Password": "12345678"},
+            "114": {"Nama": "Joni", "Email": "joni@telU.com", "Password": "12345678"},
+            "115": {"Nama": "jeje", "Email": "jeje@telU.com", "Password": "12345678"},
+        },
+    }
 
-    #key pada dict_mhs['data'] adalah NIM
-    dict_mhs = {'field' : [('Nama', "^([a-zA-Z]+([ '-]| ')?[a-zA-Z]+){1,4}$"),
-                           ('Email', '^([a-z0-9]+[._]?[a-z0-9]+)+[@]\w+[.]\w{2,3}'),
-                           ('Password', '^[a-zA-Z0-9]{8,12}$')],
-             'data' : {'113': {'Nama': 'Dummy', 'Email': 'dummy@telU.com', 'Password': '12345678'},
-                       '114': {'Nama': 'Joni', 'Email': 'joni@telU.com', 'Password': '12345678'},
-                       '115': {'Nama': 'jeje', 'Email': 'jeje@telU.com', 'Password': '12345678'}
+    # Value pada key 'Activities' berupa list berisi id_activity
+    list_topic = [
+        {
+            "Title": "Dummy Topic 1",
+            "Description": "Ini deskripsi topic 1",
+            "Activities": [0, 1],
+        },
+        {
+            "Title": "Dummy Topic 2",
+            "Description": "Ini deskripsi topic 2",
+            "Activities": [2],
+        },
+    ]
 
-                       }           
-            }
+    # key pada dict_activity adalah id_activity
+    dict_activity = {
+        0: {
+            "Title": "Dummy Assignment 1",
+            "Type": "assignment",
+            "Description": "buatlah program Game",
+        },
+        1: {
+            "Title": "Dummy material",
+            "Type": "material",
+            "Description": "slide minggu ini",
+        },
+        2: {
+            "Title": "Dummy Assignment 2",
+            "Type": "assignment",
+            "Description": "buatlah program LMS",
+        },
+    }
 
-    #Value pada key 'Activities' berupa list berisi id_activity
-    list_topic = [{'Title': 'Dummy Topic 1', 'Description': 'Ini deskripsi topic 1', 'Activities':[0, 1]},
-                    {'Title': 'Dummy Topic 2', 'Description': 'Ini deskripsi topic 2', 'Activities':[2]}
-                  ]
+    # key pada dict_submission adalah id_activity
+    dict_submission = {
+        0: {
+            "113": "Ini jawaban mahasiswa 113",
+            "114": "Ini jawaban mahasiswa 114"
+        },
+        2: {
+            "113": "Ini jawaban mahasiswa 113",
+            "114": "Ini jawaban mahasiswa 114"
+        },
+    }
 
-    # key pada dict_activity adalah id_activity 
-    dict_activity = {0: {'Title': 'Dummy Assignment 1', 'Type': 'assignment', 'Description': 'buatlah program Game'},
-                         1: {'Title': 'Dummy material', 'Type': 'material', 'Description': 'slide minggu ini'},
-                         2: {'Title': 'Dummy Assignment 2', 'Type': 'assignment', 'Description': 'buatlah program LMS'}
-                         }
-
-    # key pada dict_submission adalah id_activity 
-    dict_submission = {0: {'113' : 'Ini jawaban mahasiswa 113',
-                           '114': 'Ini jawaban mahasiswa 114'},
-                        2: {'113' : 'Ini jawaban mahasiswa 113',
-                           '114': 'Ini jawaban mahasiswa 114'}
-                       }
-
-    # key pada dict_grade adalah id_activity 
-    dict_grade = {0: {'113' : 100}
-                        
-                  }
+    # key pada dict_grade adalah id_activity
+    dict_grade = {0: {"113": 100, "114": 500}}
 
   
 
